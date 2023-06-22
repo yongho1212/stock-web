362a11761f4f826c9384478e8cfe355807c13eb9
yong <yongho6800@gmail.com>
2023-06-22 17:32:56 +0900

    theme

---
+++ src/state/theme/themeSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const lightTheme = {
+  "--100-color": "#F0F4F8",
+  "--200-color": "#D9E2EC",
+  "--300-color": "#BCCCDC",
+  "--400-color": "#9FB3C8",
+  "--500-color": "#829AB1",
+  "--600-color": "#627D98",
+  "--700-color": "#334E68",
+  "--800-color": "#243B53",
+  "--900-color": "#102A43",
+};
+
+const darkTheme = {
+  "--900-color": "#F0F4F8",
+  "--800-color": "#D9E2EC",
+  "--700-color": "#BCCCDC",
+  "--600-color": "#9FB3C8",
+  "--500-color": "#829AB1",
+  "--400-color": "#627D98",
+  "--300-color": "#334E68",
+  "--200-color": "#243B53",
+  "--100-color": "#102A43",
+};
+
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState: {
+    currentTheme: lightTheme,
+  },
+  reducers: {
+    toggleTheme: (state) => {
+      if (state.currentTheme === lightTheme) {
+        state.currentTheme = darkTheme;
+      } else {
+        state.currentTheme = lightTheme;
+      }
+    },
+  },
+});
+
+export const { toggleTheme } = themeSlice.actions;
+export default themeSlice.reducer;
