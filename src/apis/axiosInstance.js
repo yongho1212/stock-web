22e4258175f874c6bfe19f6ee00c499337be5218
yong <yongho6800@gmail.com>
2023-06-28 13:31:30 +0900

    separation version

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://stock-prj-server-1a605441f6ae.herokuapp.com/',
-  baseURL: 'http://localhost:5001/',
+  baseURL: 'https://stock-prj-server-1a605441f6ae.herokuapp.com/',
+  // baseURL: 'http://localhost:5001/',
 
   
 });
