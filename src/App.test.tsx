3e849d53dd81516cd349eefe56c0bc992eb2e4f5
최용호 <yongho6800@gmail.com>
2023-09-14 17:21:41 +0900

    url

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders header component', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const headerElement = screen.getByText(/서버에서 주식 데이터를 받아오고 있습니다/i);
+  expect(headerElement).toBeInTheDocument();
 });
